307dbbb2df134d816b9e06ae86b6453db677b60c
sinamoraddar <sinamoraddar@gmail.com>
2020-08-14 14:25:25 +0430

    create the base of hedaer

---
+++ src/App/Components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+//styles
+import styles from "./Header.module.scss";
+
+const Header = () => {
+  return <div className={styles.header}>header</div>;
+};
+
+export default Header;
